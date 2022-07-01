56aecc90635abb8b98dbb4034b858054aaf12d09
Stephan <s2036096@vuw.leidenuniv.nl>
2022-07-01 10:26:05 +0200

    SVM with 96% accuracy

--- Digit_Recognizer/SVM_DigitRec.py
+++ Digit_Recognizer/SVM_DigitRec.py
@@ -10,18 +10,19 @@ def main():
     df = pd.read_csv ('data/train.csv')
     matrix = df.to_numpy()
     number_of_rows = matrix.shape[0]
-    indices = np.random.choice(number_of_rows, size=10000, replace=False)
+    indices = np.random.choice(number_of_rows, size=20000, replace=False)
 
     matrix = matrix[indices]
 
     y = matrix[:,0]
     x = matrix[:,1:]
     X = x/255.0
-    X_scaled = scale(X)
+    X_scaled = X
+    #scale(X)
 
     X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.33, random_state=42)
 
-    clf = SVC(gamma=0.001)
+    clf = SVC(C=100,gamma=0.01, kernel='rbf')
     clf.fit(X_train,y_train)
 
     y_pred = clf.predict(X_test)
